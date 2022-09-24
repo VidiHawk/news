f01a3ffff035e63abe561a599a923af0225def61
VidiHawk <fabian@demortier.net>
2022-09-24 19:56:28 +0200

    added additional news outles

--- service.py
+++ service.py
@@ -14,72 +14,88 @@ from datetime import datetime
 import schedule
 import time
 
+irish_news = {
+    "The Independent": "https://www.independent.ie/",
+    "RTE": "https://www.rte.ie/",
+    "The Irish Time": "https://www.irishtimes.com/",
+    "The Irish Mirror": "https://www.irishmirror.ie/",
+    "The BBC": "https://www.bbc.com/",
+    "The Daily Mail": "https://www.dailymail.co.uk/",
+    "The Sun": "https://www.thesun.ie/",
+    "The Guardian": "https://www.theguardian.com/",
+    "The Irish Examiner": "https://www.irishexaminer.com/",
+    "The Journal": "https://www.thejournal.ie/",
+}
+
 
 date = datetime.today().strftime("%Y-%m-%d")
+data = []
+urls_set = set()
 
 ###################################################################################
 # Get 20 most important articles
 ###################################################################################
 
-articles = []
-urls_set = set()
-independent_articles = build("https://www.independent.ie/", memoize_articles=False)
-for article in independent_articles.articles:
-    # check to see if the article url doesn't already exist in the list
-    # and that the url doesn't lead to a video
-    if article.url not in urls_set:
-        urls_set.add(article.url)
-        articles.append(article.url)
 
-twenty = articles[:20]
+def ireland():
+    print("\nI'm working...")
 
-# for i in twenty:
-#     print(i)
+    for key, value in irish_news.items():
+        news_outlet = key
+        print("\nnewspaper: ", news_outlet)
+        articles = []
+        independent_articles = build(value, memoize_articles=False)
+        for article in independent_articles.articles:
+            # check to see if the article url doesn't already exist in the list
+            # and that the url doesn't lead to a video
+            if article.url not in urls_set:
+                urls_set.add(article.url)
+                articles.append(article.url)
+            if len(articles) == 20:
+                break
 
-####################################################################################
-# Retrieve metadata and text for each article
-####################################################################################
+        ### Retrieve metadata and text for each article ###
+
+        for i in articles:
+            my_article = Article(i, language="en")
+            my_article.download()
+            my_article.parse()
+            my_article.nlp()
+
+            url = i
+            title = my_article.title
+            text = TextBlob(my_article.text)
+            keywords = my_article.keywords
+
+            ### Perform sentiment analysis ###
+            # Subjectivity is the output that lies within [0,1] and refers to personal opinions and judgments.
+            # Polarity is the output that lies between [-1,1], where -1 refers to negative sentiment and +1 refers to positive sentiment.
+
+            polarity = text.sentiment.polarity
+            subjectivity = text.sentiment.subjectivity
+
+            # print("\nurl: ", url)
+            print("title: ", title)
+            # print("\nkeywords: ", keywords)
+            # print("\npolarity: ", polarity)
+            # print("\nsubjectivity: ", subjectivity)
+            # print("\nsummary: ", my_article.summary)
+
+            data.append(
+                [date, news_outlet, url, title, text, keywords, polarity, subjectivity]
+            )
+
+        print("\ntime: ", datetime.now())
+        print("\nnumber of articles in the database: ", len(data))
+    return
 
 
-data = []
+# schedule.every().day.at("21:00").do(ireland)
+schedule.every(1).minutes.do(ireland)
 
-for i in twenty:
-    my_article = Article(i, language="en")
-    my_article.download()
-    my_article.parse()
-    my_article.nlp()
-
-    url = i
-    title = my_article.title
-    text = TextBlob(my_article.text)
-    keywords = my_article.keywords
-
-    ### Perform sentiment analysis ###
-    # Subjectivity is the output that lies within [0,1] and refers to personal opinions and judgments.
-    # Polarity is the output that lies between [-1,1], where -1 refers to negative sentiment and +1 refers to positive sentiment.
-
-    polarity = text.sentiment.polarity
-    subjectivity = text.sentiment.subjectivity
-
-    print("\nurl: ", url)
-    print("\ntitle: ", title)
-    print("\nkeywords: ", keywords)
-    print("\npolarity: ", polarity)
-    print("\nsubjectivity: ", subjectivity)
-    print("\nsummary: ", my_article.summary)
-
-    data.append([date, url, title, text, keywords, polarity, subjectivity])
-
-
-# def job(t):
-#     print("I'm working...", t)
-#     return
-
-# schedule.every().day.at("20:00").do(job, "It is 20:00")
-
-# while True:
-#     schedule.run_pending()
-#     time.sleep(60)  # wait one minute
+while True:
+    schedule.run_pending()
+    time.sleep(60)  # wait one minute
 
 
 # run it with nohup, e.g.: nohup python3 service.py &
